7ca71815022b26d7e3f06cb81bf69dcc1e0ea8d9
Config Sync Team <noreply@google.com>
2023-10-11 15:45:58 -0700

    refactor: move controller logic to package
    This is a non-functional change that moves the controller instantiation
    logic to an importable package. This is intended to make it easier to
    import and reuse the controller.
    
    b/305080617
    
    Change-Id: I52f6249b3ff83d9c8a1199ff2ed04c336738ce98
    GitOrigin-RevId: d00cf794e37580c11ead64798d840bd8d1037d80

--- main.go
+++ main.go
@@ -15,127 +15,12 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
 
-	"github.com/go-logr/logr"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	"kpt.dev/resourcegroup/apis/kpt.dev/v1alpha1"
-	"kpt.dev/resourcegroup/controllers/log"
-	ocmetrics "kpt.dev/resourcegroup/controllers/metrics"
-	"kpt.dev/resourcegroup/controllers/profiler"
-	"kpt.dev/resourcegroup/controllers/resourcegroup"
-	"kpt.dev/resourcegroup/controllers/resourcemap"
-	"kpt.dev/resourcegroup/controllers/root"
-	"kpt.dev/resourcegroup/controllers/typeresolver"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/event"
+	"kpt.dev/resourcegroup/controllers/runner"
 	// +kubebuilder:scaffold:imports
 )
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
 func main() {
-	err := run()
-	if err != nil {
-		setupLog.Error(err, "exiting")
-		os.Exit(1)
-	}
-}
-
-func run() error {
-	_ = clientgoscheme.AddToScheme(scheme)
-
-	_ = v1alpha1.AddToScheme(scheme)
-	// +kubebuilder:scaffold:scheme
-
-	_ = apiextensionsv1.AddToScheme(scheme)
-
-	log.InitFlags()
-
-	var metricsAddr string
-	var enableLeaderElection bool
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.Parse()
-
-	profiler.Service()
-
-	// Register the OpenCensus views
-	if err := ocmetrics.RegisterReconcilerMetricsViews(); err != nil {
-		return fmt.Errorf("failed to register OpenCensus views: %w", err)
-	}
-
-	// Register the OC Agent exporter
-	oce, err := ocmetrics.RegisterOCAgentExporter()
-	if err != nil {
-		return fmt.Errorf("failed to register the OC Agent exporter: %w", err)
-	}
-
-	defer func() {
-		if err := oce.Stop(); err != nil {
-			setupLog.Error(err, "Unable to stop the OC Agent exporter")
-		}
-	}()
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "413d8c8e.gke.io",
-	})
-	if err != nil {
-		return fmt.Errorf("failed to build manager: %w", err)
-	}
-
-	logger := ctrl.Log.WithName("controllers")
-
-	for _, group := range []string{root.KptGroup} {
-		registerControllersForGroup(mgr, logger, group)
-	}
-
-	// +kubebuilder:scaffold:builder
-
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		return fmt.Errorf("failed to start controller-manager: %w", err)
-	}
-	return nil
-}
-
-func registerControllersForGroup(mgr ctrl.Manager, logger logr.Logger, group string) {
-	// channel is watched by ResourceGroup controller.
-	// The Root controller pushes events to it and
-	// the ResourceGroup controller consumes events.
-	channel := make(chan event.GenericEvent)
-
-	setupLog.Info("adding the type resolver")
-	resolver, err := typeresolver.NewTypeResolver(mgr, logger.WithName("TypeResolver"))
-	if err != nil {
-		setupLog.Error(err, "unable to set up the type resolver")
-		os.Exit(1)
-	}
-	resolver.Refresh()
-
-	setupLog.Info("adding the Root controller for group " + group)
-	resMap := resourcemap.NewResourceMap()
-	if err := root.NewController(mgr, channel, logger.WithName("Root"), resolver, group, resMap); err != nil {
-		setupLog.Error(err, "unable to create the root controller for group "+group)
-		os.Exit(1)
-	}
-
-	setupLog.Info("adding the ResourceGroup controller for group " + group)
-	if err := resourcegroup.NewRGController(mgr, channel, logger.WithName(v1alpha1.ResourceGroupKind), resolver, resMap, resourcegroup.DefaultDuration); err != nil {
-		setupLog.Error(err, "unable to create the ResourceGroup controller")
-		os.Exit(1)
-	}
+	os.Exit(runner.Run())
 }
